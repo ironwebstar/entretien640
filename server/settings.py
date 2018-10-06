950dca5498ee76259628b2f2ebacc453eb9d1725
webcodestar <webcodestar@gmail.com>
2018-10-06 23:04:01 +1000

    10.6-updated

--- server/settings.py
+++ server/settings.py
@@ -28,7 +28,7 @@ DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 ALLOWED_HOSTS = ['floating-brook-18937.herokuapp.com', '192.168.1.77', ]
 CORS_ORIGIN_WHITELIST = (
     '192.168.1.112:4200',
-    'foregoing-money.surge.sh',
+    'abusive-profit.surge.sh',
 )
 
 # Application definition
